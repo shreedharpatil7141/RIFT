7cf201e63831b2a1b1caad7479520f60147cae24
shreedharpatil7141 <shreedhargoudapatil1@gmail.com>
2026-02-20 11:58:37 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # ✅ Enable CORS for frontend (Next.js runs on 3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://rift-fk5d.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
